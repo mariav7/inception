968111c890f35d0f8a7130eab3e418d9bb43686c
mflores- <mflores-@student.42.fr>
2023-10-06 14:34:02 +0200

    fix: deleted old files

--- anonymize-login.sh
+++
@@ -1,3 +0,0 @@
-sed -i "s/${LOGIN}/login/g" Makefile
-sed -i "s/${LOGIN}/login/g" srcs/.env
-sed -i "s/${LOGIN}/login/g" srcs/requirements/nginx/conf/default.conf
--- configure-login.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed -i "s/login/${LOGIN}/g" srcs/.env
-sed -i "s/login/${LOGIN}/g" srcs/requirements/nginx/conf/default.conf
