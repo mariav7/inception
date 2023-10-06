acdb875fd74318b8e86af6e5aa147055ed6261d1
mflores- <mflores-@student.42.fr>
2023-10-06 14:36:01 +0200

    fix: script

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -7,7 +7,6 @@
 sleep 10
 if [ ! -f /var/www/html/wp-config.php ]; then
     cd /var/www/html
-    #rm -rf *
     wp core download --allow-root
     wp config create --dbname=${BDD_NAME} --dbuser=${BDD_USER} --dbpass=${BDD_USER_PASSWORD} --dbhost=${BDD_HOST} --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
