367e6bd98a55c0eff6715ac0e6e947ef03695918
mflores- <mflores-@student.42.fr>
2023-10-06 13:10:27 +0200

    to be finished

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+# if [ ! -f /var/www/html/wp-config.php ]; then
+#     until mysqladmin -hmariadb -u root -p${BDD_ROOT_PASSWORD} ping; do
+#         sleep 2
+#     done
+sleep 10
 if [ ! -f /var/www/html/wp-config.php ]; then
-    until mysqladmin -hmariadb -u${WP_ADMIN_USER} -p${WP_ADMIN_PASSWORD} ping; do
-        sleep 2
-    done
-        cd /var/www/html
-        rm -rf *
-        wp core download --allow-root
-        wp config create --dbname=${BDD_NAME} --dbuser=${BDD_USER} --dbpass=${BDD_USER_PASSWORD} --dbhost=${BDD_HOST} --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-        wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${BDD_USER} --admin_password=${BDD_USER_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
-        wp user create ${WP_USER} ${WP_USER_EMAIL} --role=${WP_USER_ROLE} --user_pass=${WP_USER_PASSWORD} --allow-root 
+    cd /var/www/html
+    #rm -rf *
+    wp core download --allow-root
+    wp config create --dbname=${BDD_NAME} --dbuser=${BDD_USER} --dbpass=${BDD_USER_PASSWORD} --dbhost=${BDD_HOST} --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+    wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
+    wp user create ${WP_USER} ${WP_USER_EMAIL} --role=${WP_USER_ROLE} --user_pass=${WP_USER_PASSWORD} --allow-root 
 fi
 
 php-fpm7.4 -F -R
